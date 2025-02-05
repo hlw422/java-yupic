c061cbea67fefb72aef48f0eb5a7057d8e9940b9
hlw422 <hlw422@126.com>
2025-02-05 13:49:45 +0800

    修改页面结构，少拷贝文件夹修复

---
+++ yupipictureend/src/main/java/com/hlw/yupipictureend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.hlw.yupipictureend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hlw.yupipictureend.entity.User;
+
+/**
+* @author lwh
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-02-02 14:31:16
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
